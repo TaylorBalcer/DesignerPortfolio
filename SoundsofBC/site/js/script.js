2f6da07ccfe0b46dabe5eb8ededb12f856698947
TaylorBalcer <tjb42@students.ptcollege.edu>
2022-05-15 19:30:56 -0400

    fixing links

---
+++ SoundsofBC/site/js/script.js
@@ -0,0 +1,68 @@
+// /* JavaScript */
+
+// //make short var reference to the audio player:
+
+// let audioElement = document.querySelectorAll("audio")[3];
+
+
+// //Array sotring the track titles
+// let titles = [
+//     "WALKING THROUGH THE CITY",
+//     "TAKING THE SKYTRAIN",
+//     "TRAVELING ON THE FERRY"
+
+// ];
+
+// let soundPlay =[
+//     "audio/sunrisemarket_mixdown.mp3",
+//     "audio/trainsound_mixdown.mp3",
+//     "audio/burrardinlet_mixdown.mp3",
+//   ];
+
+
+// let whichTrack= 0;
+// let whichSound = 0;
+// document.querySelector("#title_text").innerHTML = titles[whichTrack];
+
+// const audioNext = () => {
+//     whichTrack = (whichTrack + 1 > titles.length - 1)? 0 : whichTrack + 1;
+    
+//     document.querySelector("#title_text").innerHTML = titles[whichTrack];
+  
+  
+//    whichSound = (whichSound + 1 > soundPlay.length - 1) ? 0 : whichSound + 1;
+//    audioElement.src = soundPlay[whichTrack]; 
+  
+//    document.querySelectorAll("audio")[3].load();
+//   document.querySelectorAll("audio")[3].play();
+//   }
+//   document.querySelector("#next_btn").addEventListener("click", audioNext);
+
+// const audioNext = () => {
+//     whichTrack++;
+//     if (whichTrack > titles.length -1) whichTrack = 0;
+//     document.querySelector("#title_text").innerHTML = titles[whichTrack];
+
+// }
+// document.querySelector("#next_btn").addEventListener("click", audioNext);
+
+// const audioPlay= () => {
+//     //alert("audioPlay");
+//     audioElement.play();
+// }
+
+// //Event listener: clicking the button excutes a function
+// document.querySelector("#play_btn").addEventListener("click", audioPlay);
+
+
+
+// const audioPause= () => {
+//     //alert("audioPause");
+//     audioElement.pause();
+// }
+
+// //Event listener: clicking the button excutes a function
+// document.querySelector("#pause_btn").addEventListener("click", audioPause);
+
+
+
